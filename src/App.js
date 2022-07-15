47ccac43ad2218b2932d9c83a0de8182b90ed39d
Anubhav <chakrabortyanubhav0@gmail.com>
2022-07-15 23:59:41 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,150 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState,useEffect } from 'react';
+import Popular from './components/Popular';
+
+
+function App() {
+const [value,setValue]=useState("");
+const [food,setFood]=useState()
+
+
+const handlecsearch=async(c,m,d)=>{
+  
+  const data=await fetch(`https://api.edamam.com/search?q=${value}&from=0&to=100&dishType=${d}&mealType=${m}&cuisineType=${c}&app_id=c506cb45&app_key=785aa09ed1af4b122448dd6aff9f24b9`)
+  const rdata=await data.json()
+  setFood(rdata.hits);
+  if(rdata.hits.length===0){
+    alert('No Results Found')
+  }
+  console.log(rdata.hits)
+
+}
+const handleChange=(e)=>{
+  setValue(e.target.value)
+}
+
+const handleClick=async()=>{
+  console.log(value)
+  const data=await fetch(`https://api.edamam.com/search?q=${value}&from=0&to=100&$app_id=c506cb45&app_key=785aa09ed1af4b122448dd6aff9f24b9`)
+    const rdata=await data.json()
+    setFood(rdata.hits);
+    if(rdata.hits.length===0){
+      alert('No Results Found')
+    }
+    console.log(rdata.hits)
+    
+  }
+  return (
+    <div className="App">
+<nav className="navbar navbar-dark bg-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+      <img src="https://cdn-icons-png.flaticon.com/128/3075/3075977.png"  alt="" width="30" height="24" className="d-inline-block align-text-top"/>
+      &nbsp;RecipeNinja
+    </a>
+  </div>
+</nav>
+
+
+{/*Sea,rch bar */}
+
+<div className="container my-2">
+
+<div className="row my-2">
+<div className="searchcont">
+<i className="fa fa-search fa-2x mx-1 " onClick={handleClick}></i>
+<input type="text" placeholder="search" id="searchtext" value={value} onChange={handleChange} style={{height:"7vh"}} />
+</div>
+</div>
+
+<div className="row secsearch">
+ <div className="col-12">
+  <h3 style={{fontWeight:"bolder",fontFamily:"sans-serif",textShadow:"1px 1px 2px white"}}>Search By Category:-</h3>
+ </div>
+</div>
+<div className="row my-2 secsearch">
+<div className="mx-auto col-sm-3 my-2">
+  <label for="mealtype">Meal:</label><select id="mealtype">
+  
+    <option value={"Breakfast"}>Breakfast</option>
+    <option value="Lunch">Lunch</option>
+    <option value="Dinner">Dinner</option>
+    <option value="Snack">Snack</option>
+    <option value="Teatime">Teatime</option>
+  </select>
+</div>
+<div className="mx-auto col-sm-3 my-2">
+<label for="dishtype">Dish:</label><select id="dishtype" >
+    <option value="alcohol cocktail">Alcohol Cocktail</option>
+    <option value="biscuits and cookies">Biscuit and Cookies</option>
+    <option value={"Bread"}>Bread</option>
+    <option value="cereals">Cereals</option>
+    <option value="condiments and sauces">Condiments and Sauces</option>
+    <option value="desserts">Desserts</option>
+    <option value="drinks">Drinks</option>
+    <option value="soup">Soup</option>
+    <option value="starter">starter</option>
+    <option value="seafood">Seafood</option>
+    <option value="sandwiches">Sandwiches</option>
+    <option value="pasta">Pasta</option>
+    <option value="salad">Salad</option>
+    <option value="main course">Main Course</option>
+    <option value="pastry">Pastry</option>
+    <option value="pies and tarts">Pies and Tarts</option>
+    <option value="sweets">Sweets</option>
+    <option value="ice cream and custard">Ice Cream and Custard</option>
+   
+ 
+  </select>
+</div>
+
+
+<div className="mx-auto col-sm-3 my-2">
+<label for="cuisinetype">Cuisine:</label><select id="cuisinetype">
+
+    <option value={"american"}>American</option>
+    <option value="asian">Asian</option>
+    <option value="british">British</option>
+    <option value="caribbean">Caribbean</option>
+    <option value="central europe">Central Europe</option>
+    <option value="eastern europe">Eastern Europe</option>
+    <option value="chinese">Chinese</option>
+    <option value="french">French</option>
+    <option value="indian">Indian</option>
+    <option value="italian">Italian</option>
+    <option value="japanese">Japanese</option>
+    <option value="mediterranean">Mediterranean</option>
+    <option value="mexican">Mexican</option>
+    <option value="middle eastern">Middle Eastern</option>
+    <option value="nordic">Nordic</option>
+    <option value="south american">South American</option>
+    <option value="south east asian">South East Asian</option>
+
+  </select>
+
+
+</div>
+</div>
+
+<div className="row secsearch">
+<div className="col-md-6 mx-auto my-3">
+  <button className='btn btn-dark btn-block w-100' onClick={()=>{
+  console.log(typeof(document.getElementById('cuisinetype').value))
+  console.log(document.getElementById('mealtype').value)
+  console.log(document.getElementById('cuisinetype').value)
+  handlecsearch(document.getElementById('cuisinetype').value,document.getElementById('mealtype').value,document.getElementById('dishtype').value);
+}}>Search..</button>
+</div>
+</div>
+ 
+      </div>
+
+<Popular food={food}/>
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ src/components/Popular.js
@@ -0,0 +1,116 @@
+import React, { useEffect } from 'react'
+
+
+export default function Popular(props) {
+
+const modall=(id)=>{
+  document.getElementById(`${id}`).showModal();
+  
+}
+
+  
+  return (
+    <div className='container displaycont'>
+      <div className="row">
+     
+      {props?.food?.map((item,i)=>{
+        return ("nodata" && 
+        
+        <div className="col-md-3 mx-auto rcont bg-warning my-3" style={{padding:"0px",boxShadow:"1px 1px 1px black,-1px 1px 2px black"}}>
+          
+          <div class="card p-0 mx-auto" style={{width:"100%",height:"100%"}}>
+          <div className="imgwrap" style={{height:"170px"}} >
+          
+    <img style={{width:"100%",height:"100%"}} src={item.recipe.image} alt="No image to show"/>
+    </div>
+    <div class="card-body" style={{textAlign:"left"}}>
+      <p class="card-text  my-1"><strong>Recipe Name-{item.recipe.label}</strong></p>
+      <p class="card-text my-1"><strong>Cuisine Type-{item.recipe.cuisineType[0]}</strong></p>
+      <p class="card-text my-1"><strong>Meal Type-{item.recipe.mealType[0]}</strong></p>
+    </div>
+    
+    <dialog id={`${i}`} style={{width:"50rem"}}>
+    <div className="container-fluid table-responsive" style={{width:"100%"}} >
+    <h3><strong style={{textAlign:"centre",fontWeight:"bolder",fontFamily:"cursive"}}>{item.recipe.label}</strong></h3>
+    <table class="table table-striped table-dark mt-3" style={{width:"100%"}}>
+    <thead>
+    <tr>
+    <th scope="col">Ingredient</th>
+    <th scope='col'>Quantity</th>
+    <th scope='col'>Weight</th>
+    <th scope='col'>Measure</th>
+    <th scope='col'>Ingredient Category</th>
+    </tr>
+    </thead>
+    <tbody className='table-warning'>
+    {item.recipe.ingredients.map((ig)=>{
+return (
+
+
+
+<tr>
+<td scope="col">{ig.text}</td>
+<td scope="col">{ig.quantity}</td>
+<td scope="col">{ig.weight.toFixed(2)}&nbsp;grams</td>
+<td scope="col">{ig.measure}</td>
+<td scope="col">{ig.foodCategory}</td>
+</tr>
+
+  
+
+)
+    })}
+    </tbody>
+    </table>
+    
+    <br/>
+    <h4><strong style={{fontFamily:"cursive"}}>Info:-</strong></h4>
+    <table className='table table-dark table-striped '>
+      <thead className='table-dark'>
+      <tr>
+<th scope="col">Calories</th>
+<th scope="col">Total Weight</th>
+<th scope="col">Yield</th>
+<th scope="col">Cooking Time</th>
+</tr>
+      </thead>
+   
+      <tbody className='table-warning'>
+      <tr>
+        <td scope="col">{item.recipe.calories.toFixed(2)}</td>
+        <td scope="col">{item.recipe.totalWeight.toFixed(2)}grams</td>
+        <td scope="col">{item.recipe.yield}</td>
+        <td scope="col">{item.recipe.totalTime} &nbsp;mins</td>
+        </tr>
+      </tbody>
+      
+    </table>
+    
+    <div className="w-100 mx-auto my-2" id="mybutton">
+    <button   className='btn btn-danger' style={{width:"100%"}} onClick={()=>{
+      document.getElementById(`${i}`).close()
+    }}>Close</button>
+    </div>
+    
+    </div>
+
+  
+    </dialog>
+ <button className='btn btn-outline-warning btn-block' onClick={()=>{
+modall(i);
+ }}>Ingredients</button>
+
+    <a href={item.recipe.url} className='btn btn-outline-danger btn-block' >Full Recipe</a>
+  </div>
+          </div>
+        
+        )
+      },)}
+
+
+
+
+      </div>
+    </div>
+  )
+}
